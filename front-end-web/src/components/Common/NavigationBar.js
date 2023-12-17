caebcc9112335afb80f1921bd33d9b4d55b0d4af
Phongbui <phongprosupper@gmail.com>
2023-12-17 21:03:27 +0700

    update NavigationBar,..

---
+++ front-end-web/src/components/Common/NavigationBar.js
@@ -0,0 +1,60 @@
+// components/Common/NavigationBar.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+const NavigationBar = () => {
+  // State to track whether the user is logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    // Perform logout actions and update the state
+    setIsLoggedIn(false);
+    // Additional logout logic if needed
+  };
+
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about">About</Link>
+        </li>
+        {/* Add more navigation links as needed */}
+      </ul>
+      <div className="buttons-container">
+        {isLoggedIn ? (
+          <>
+            <div className="logout-button">
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+            <div className="user-info">
+              {/* Display user information or profile picture */}
+              User Info
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="login-button">
+              <Link to="/login">Login</Link>
+            </div>
+            <div className="register-button">
+              <Link to="/register">Register</Link>
+            </div>
+          </>
+        )}
+        <div className="currency-button">
+          {/* Add your currency selection logic or dropdown here */}
+          Currency
+        </div>
+        <div className="country-icon">
+          {/* Add your country icon or flag here */}
+          Country Icon
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
--- front-end-web/src/pages/Home/HomePage.js
+++ front-end-web/src/pages/Home/HomePage.js
@@ -1,55 +1,28 @@
-import React, { useState } from 'react';
+// components/pages/HomePage.js
+import React from 'react';
 import { Link } from 'react-router-dom';
-// import './HomePage.css'; // Import your stylesheet if needed
+import './HomePage.css'; // Import your stylesheet if needed
 
 const HomePage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [selectedRole, setSelectedRole] = useState('user'); // Default role is set to 'user'
-
-  const handleLogin = () => {
-    // Implement your login logic here
-    // You may use a state management library (e.g., Redux) or make an API call to authenticate the user
-    console.log('Logging in with:', { username, password, role: selectedRole });
-  };
-
   return (
-    <div className="home-page">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple example of a home page in your React application.</p>
-      <p>Feel free to customize and add more content.</p>
+    <div className="home-page-container">
+      <header className="header">
+        <h1>Welcome to Your Website</h1>
+      </header>
 
-      <div className="login-form">
-        <h2>Login</h2>
-        <form>
-          <label>
-            Username:
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          </label>
-          <label>
-            Password:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </label>
-          <label>
-            Role:
-            <select value={selectedRole} onChange={(e) => setSelectedRole(e.target.value)}>
-              <option value="admin">Admin</option>
-              <option value="user">User</option>
-              <option value="hotel_owner">Hotel Owner</option>
-              <option value="manager">Manager</option>
-            </select>
-          </label>
-          <button type="button" onClick={handleLogin}>
-            Login
-          </button>
-        </form>
-      </div>
+      <section className="main-content">
+        <p>This is a simple and beautiful home page for your React application.</p>
+        <p>Feel free to customize and add more content.</p>
 
-      <div className="navigation-links">
-        {/* Example navigation links */}
-        <Link to="/about">About Us</Link>
-        <Link to="/contact">Contact Us</Link>
-      </div>
+        <div className="cta-buttons">
+          <Link to="/about" className="cta-button">
+            Learn More
+          </Link>
+          <Link to="/contact" className="cta-button">
+            Contact Us
+          </Link>
+        </div>
+      </section>
     </div>
   );
 };
