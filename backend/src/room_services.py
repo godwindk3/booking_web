48e42786af619c81dfeffa2bf375358865b1a9ac
anbish <minhanthongminh@gmail.com>
2023-12-20 00:00:48 +0700

    Room CRUD

---
+++ backend/src/room_services.py
@@ -0,0 +1,71 @@
+from database import SessionLocal
+import data_models
+import validation_models
+from fastapi import HTTPException, status
+
+db = SessionLocal()
+
+
+def get_all_rooms():
+    return db.query(data_models.Room).all()
+
+def get_room_by_id(room_id):
+    temp = db.query(data_models.Room).filter(data_models.Room.id == room_id).first()
+    if (temp is None):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Room with ID = {room_id} does not exists.")
+    
+    return temp
+
+def get_rooms_by_accommodation_id(accommodation_id: int):
+    return db.query(data_models.Room).filter(data_models.Room.accommodationID == accommodation_id).all()
+
+
+def get_specific_room_by_acco_and_room_number(accommodation_id: int, room_number: int):
+    return db.query(data_models.Room).filter(data_models.Room.accommodationID == accommodation_id, data_models.Room.room_number == room_number).first()
+
+
+def create_room(room: validation_models.Room):
+    new_room = data_models.Room(
+        accommodationID=room.accommodationID,
+        room_number=room.room_number,
+        capacity=room.capacity,
+        price=room.price,
+        status=room.status,
+        tier=room.tier
+    )
+
+    temp = db.query(data_models.Room).filter(data_models.Room.accommodationID ==
+                                             new_room.accommodationID, data_models.Room.room_number == new_room.room_number).first()
+
+    if (temp is not None):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Room with Accommodation ID = {new_room.accommodationID} and Room ID = {new_room.id} already exists.")
+
+    db.add(new_room)
+    db.commit()
+
+    return new_room
+
+def update_room(room_id: int, room: validation_models.Room):
+    room_to_update = get_room_by_id(room_id)
+
+    room_to_update.accommodation_id = room.accommodationID
+    room_to_update.room_number = room.room_number
+    room_to_update.capacity = room.capacity
+    room_to_update.price = room.price
+    room_to_update.tier = room.tier
+    room_to_update.status = room.status
+
+    db.commit()
+
+    return room_to_update 
+
+
+def delete_room(room_id):
+    room_to_del = get_room_by_id(room_id)
+
+    db.delete(room_to_del)
+    db.commit()
+
+    return room_to_del
+
