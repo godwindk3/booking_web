e4bbd7c08d96eb1c274a913eea4d2195e2e50659
Hieuvo1203 <hievoace123@gmail.com>
2023-12-25 16:45:41 +0700

    update payment

--- backend/services/payment_services.py
+++ backend/services/payment_services.py
@@ -13,3 +13,51 @@ from fastapi import HTTPException, status
 
 db = SessionLocal()
 
+def get_all_payments():
+    return db.query(data_models.Payment).all()
+
+def get_payment_by_id(id):
+    temp = db.query(data_models.Payment).filter(data_models.Payment.id == id).first()
+    
+    if (temp is None):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Payment with ID {id} does not exist")
+    
+    return temp
+
+def create_payment(payment: validation_models.Payment):
+    new_payment = data_models.Payment(
+        bookingID = payment.bookingID,
+        amount = payment.amount,
+        payment_date = payment.payment_date,
+        payment_method = payment.payment_method
+    )
+    
+    temp = db.query(data_models.Payment.bookingID).filter(data_models.Payment.bookingID == new_payment.bookingID).first()
+    
+    if (temp is not None):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Payment with booking ID {new_payment.bookingID} aleady exists.")
+    
+    db.add(new_payment)
+    db.commit()
+    
+    return new_payment
+
+def update_payment(id: int, payment: validation_models.Payment):
+    temp = get_payment_by_id(id)
+    
+    temp.bookingID = payment.bookingID
+    temp.amount = payment.amount
+    temp.payment_date = payment.payment_date
+    temp.payment_method = payment.payment_method
+    
+    db.commit()
+    
+    return temp
+
+def delete_payment(id: int):
+    temp = get_payment_by_id(id)
+    
+    db.delete(temp)
+    db.commit()
+    
+    return temp
