b8eb10846a5e613010e2859cfb9087a5e87cd1ce
Phongbui <phongprosupper@gmail.com>
2024-01-08 11:16:27 +0700

    update Private Route for Owner and Fix OwnerDashboard

--- front-end-web/src/components/Common/NavigationBar.js
+++ front-end-web/src/components/Common/NavigationBar.js
@@ -1,16 +1,126 @@
-// components/Common/NavigationBar.js
+
+// import React, { useState, useEffect } from 'react';
+// import { NavLink, useNavigate } from 'react-router-dom';
+// import './NavigationBar.css';
+
+// const NavigationBar = ({ isLoggedIn, onLogout }) => {
+//   const [showAdditionalButton, setShowAdditionalButton] = useState(false);
+//   const [userRole, setUserRole] = useState(null);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     // Update the showAdditionalButton state based on the user's login status
+//     setShowAdditionalButton(isLoggedIn);
+
+//     // Retrieve user role from local storage
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       const [, payload] = token.split('.');
+//       const decodedPayload = JSON.parse(atob(payload));
+//       setUserRole(decodedPayload.role);
+//     }
+//   }, [isLoggedIn]);
+
+//   const handleLogout = () => {
+//     onLogout(); // Notify the higher-level component about the logout
+//     navigate('/login');
+//   };
+
+//   const handleAdditionalButtonClick = () => {
+//     // Add logic for what happens when the additional button is clicked
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <section className="Header">
+//         <div className="container flex justify-between align-center Header_container_main">
+//           <div className="flex align-center gap-40 h-full">
+//             <div className="logo">
+//               <NavLink exact to="/">
+//                 <button className="home-button">
+//                   <booking>Booking</booking>
+//                   <vn>.vn</vn>
+//                 </button>
+//               </NavLink>
+//             </div>
+
+//             <div className="flex gap-24 h-full align-center cursor-pointer Header_nav_container">
+//               <NavLink exact to="/gethotel" style={{ textDecoration: 'none' }}>
+//                 <button className="h-full flex align-center nav-button">
+//                   Tìm khách sạn
+//                 </button>
+//               </NavLink>
+//               <NavLink exact to="/about" style={{ textDecoration: 'none' }}>
+//                 <button className="h-full flex align-center nav-button">
+//                   Về chúng tôi
+//                 </button>
+//               </NavLink>
+//               <NavLink exact to="/ownerregister" style={{ textDecoration: 'none' }}>
+//                 {userRole === 1 ? (
+//                   <button className="h-full flex align-center doitac-button gap-24 nav-button">
+//                     Quản lý khách sạn của bạn
+//                   </button>
+//                 ) : (
+//                   <button className="h-full flex align-center doitac-button gap-24 nav-button">
+//                     Trở thành đối tác của chúng tôi
+//                   </button>
+//                 )}
+//               </NavLink>
+//               {isLoggedIn ? (
+//                 <>
+//                   <button className="flex align-center gap-24 login-button" onClick={handleLogout}>
+//                     Đăng xuất
+//                   </button>
+//                   {showAdditionalButton && (
+//                     <NavLink exact to="/membership" style={{ textDecoration: 'none' }}>
+//                       <button className="flex align-center gap-24 login-button">
+//                         Membership
+//                       </button>
+//                     </NavLink>
+//                   )}
+//                 </>
+//               ) : (
+//                 <NavLink exact to="/login" style={{ textDecoration: 'none' }}>
+//                   <button className="flex align-center gap-24 login-button">
+//                     Đăng nhập
+//                   </button>
+//                 </NavLink>
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </nav>
+//   );
+// };
+
+// export default NavigationBar;
+
+
+
 import React, { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import './NavigationBar.css';
 
-const NavigationBar = ({ isLoggedIn, onLogout }) => {
+const NavigationBar = ({ keyProp,isLoggedIn, onLogout }) => {
   const [showAdditionalButton, setShowAdditionalButton] = useState(false);
+  const [userRole, setUserRole] = useState(null);
+  const [logoutTrigger, setLogoutTrigger] = useState(false); // new state
   const navigate = useNavigate();
+  
 
   useEffect(() => {
     // Update the showAdditionalButton state based on the user's login status
     setShowAdditionalButton(isLoggedIn);
-  }, [isLoggedIn]);
+
+    // Retrieve user role from local storage
+    const token = localStorage.getItem('token');
+    if (token) {
+      const [, payload] = token.split('.');
+      const decodedPayload = JSON.parse(atob(payload));
+      setUserRole(decodedPayload.role);
+    }
+  }, [isLoggedIn,keyProp]); // add logoutTrigger to the dependency array
 
   const handleLogout = () => {
     onLogout(); // Notify the higher-level component about the logout
@@ -46,11 +156,19 @@ const NavigationBar = ({ isLoggedIn, onLogout }) => {
                   Về chúng tôi
                 </button>
               </NavLink>
-              <NavLink exact to="/ownerregister" style={{ textDecoration: 'none' }}>
-              <button className="h-full flex align-center doitac-button gap-24 nav-button">
-                Trở thành đối tác của chúng tôi
-              </button>
-              </NavLink>
+              {userRole === 1 ? (
+                <NavLink exact to="/ownerdashboard" style={{ textDecoration: 'none' }}>
+                  <button className="h-full flex align-center doitac-button gap-24 nav-button">
+                    Quản lý khách sạn của bạn
+                  </button>
+                </NavLink>
+              ) : (
+                <NavLink exact to="/ownerregister" style={{ textDecoration: 'none' }}>
+                  <button className="h-full flex align-center doitac-button gap-24 nav-button">
+                    Trở thành đối tác của chúng tôi
+                  </button>
+                </NavLink>
+              )}
               {isLoggedIn ? (
                 <>
                   <button className="flex align-center gap-24 login-button" onClick={handleLogout}>
@@ -82,42 +200,3 @@ const NavigationBar = ({ isLoggedIn, onLogout }) => {
 export default NavigationBar;
 
 
-
-
-
-
-  {/* <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <div className="buttons-container">
-        {isLoggedIn ? (
-          <>
-            <div className="logout-button">
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-            <div className="user-info">
-              User Info
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="login-button">
-              <Link to="/login">Login</Link>
-            </div>
-            <div className="register-button">
-              <Link to="/register">Register</Link>
-            </div>
-          </>
-        )}
-        <div className="currency-button">
-          Currency
-        </div>
-        <div className="country-icon">
-          Country Icon
-        </div>
-      </div> */}
