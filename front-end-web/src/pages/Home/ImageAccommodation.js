696f7d24f95ceb7121490b1f86e66cc3d2c518e6
ngocanhh1412 <ngngocanh141203@gmail.com>
2024-01-10 18:13:19 +0700

    css OwnerDashboard

--- front-end-web/src/pages/Home/ImageAccommodation.js
+++ front-end-web/src/pages/Home/ImageAccommodation.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axiosConfig';
 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import "./ImageAccommodation.css"
+
 const ImageAccommodation = ({ accommodationId }) => {
   const [imageData, setImageData] = useState([]);
   const [imageUrls, setImageUrls] = useState([]);
@@ -61,19 +67,42 @@ const ImageAccommodation = ({ accommodationId }) => {
     }
   };
 
+  const PrevArrow = (props) => (
+    <button {...props} className="slider-arrow slider-arrow-prev">
+      <div class="arrow-left"></div>
+    </button>
+  );
+  const NextArrow = (props) => (
+    <button {...props} className="slider-arrow slider-arrow-next">
+      <div class="arrow-right"></div>
+    </button>
+  );
+  const settings = {
+    dots: true, // Hiển thị các chấm chỉ số ở dưới slider
+    infinite: true, // Vô hạn quay lại các ảnh khi chạm đến cuối cùng
+    speed: 500, // Tốc độ chuyển đổi giữa các ảnh (ms)
+    slidesToShow: 1, // Số lượng ảnh hiển thị trên mỗi trang
+    slidesToScroll: 1, // Số lượng ảnh được chuyển đổi khi trượt slider   
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
   return (
     <div>
-      <h2>Accommodation Images</h2>
-      {imageUrls.map((imageUrl, index) => (
-        <div key={index}>
-          <img
-            src={imageUrl}
-            alt={`Accommodation Image ${index + 1}`}
-            style={{ width: '600px', height: 'auto' }}
-          />
-          <button onClick={() => deleteImage(imageData[index].id)}>Delete</button>
-        </div>
-      ))}
+      <h2 className='h2-view-image-acco-header'>Ảnh khách sạn</h2>
+     
+      <Slider {...settings} className='slider-container'>
+        {imageUrls.map((imageUrl, index) => (
+          <div key={index}>
+            <img className='slider-container-img'
+              src={imageUrl}
+              alt={`Accommodation Image ${index + 1}`}
+            />
+            <button className='delete-image-button' onClick={() => deleteImage(imageData[index].id)}>Xoá Ảnh Này</button>
+          </div>
+        ))}
+      </Slider>
+
     </div>
   );
 };
