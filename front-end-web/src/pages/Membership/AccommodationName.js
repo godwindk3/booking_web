19478f3d5799c3202287dc0fa06d845af181c90a
Phongbui <phongprosupper@gmail.com>
2024-01-11 15:15:16 +0700

    add booking button

---
+++ front-end-web/src/pages/Membership/AccommodationName.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../Home/axiosConfig';
+
+const AccommodationName = ({ accommodationId }) => {
+  const [accommodation, setAccommodation] = useState(null);
+
+  useEffect(() => {
+    const fetchAccommodationData = async () => {
+      try {
+        const response = await axios.get(`/accommodation/${accommodationId}`);
+        setAccommodation(response.data);
+      } catch (error) {
+        console.error('Error fetching accommodation data:', error);
+        // Handle error
+      }
+    };
+
+    fetchAccommodationData();
+  }, [accommodationId]);
+
+  if (!accommodation) {
+    return <div>Loading...</div>; // You can render a loading indicator or handle it differently
+  }
+
+  return (
+    <div>
+      <h4>Tên khách sạn:  {accommodation.name}</h4>
+    </div>
+  );
+};
+
+export default AccommodationName;
